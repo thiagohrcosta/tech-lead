d516eb54d2fb2d0e23cde7284a9ca27980e2a3bf
Giorgio Zuccarelli <giorgio.zuccarelli21@avenues.org>
2021-04-27 21:13:47 -0300

    created speciality model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_232309) do
+ActiveRecord::Schema.define(version: 2021_04_27_235916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,4 +35,15 @@ ActiveRecord::Schema.define(version: 2021_04_27_232309) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.bigint "role_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["role_id"], name: "index_users_on_role_id"
+  end
+
+  add_foreign_key "users", "roles"
 end
